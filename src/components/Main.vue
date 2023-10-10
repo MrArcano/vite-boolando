9ed7dce4566b82c49de71f76fe835245861b8ce5
MrArcano <salvatore-dario@hotmail.it>
2023-10-10 18:12:50 +0200

    [edit]: data card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -35,16 +35,16 @@ export default {
             </div>
             <CardHearth />
             <div class="badge-box">
-              <span class="badge badge-red">-50%</span>
-              <span class="badge badge-green">Sostenibilità</span>
+              <span v-if="card.badgeRed" class="badge badge-red">{{ card.badgeRed }}</span>
+              <span v-if="card.badgeGreen" class="badge badge-green">{{ card.badgeGreen }}</span>
             </div>
           </div>
 
           <div class="card-bot">
-            <span class="brand block-box">Levi's</span>
-            <span class="name-item block-box">Relaxed Fit Tee Unisex</span>
-            <span class="new-price">14,99&euro;</span>
-            <span class="old-price">29,99&euro;</span>
+            <span class="brand block-box">{{ card.brand }}</span>
+            <span class="name-item block-box">{{ card.text }}</span>
+            <span class="new-price">{{ card.newPrice }}&euro;</span>
+            <span v-if="card.oldPrice" class="old-price">{{ card.oldPrice }}&euro;</span>
           </div>
         </div> <!-- fine card -->
 
